5671f3ed3bf0e1fafea83a98e5b0b63c9c48109e
Dimmis5 <zhangdimeo@gmail.com>
2025-12-13 15:25:39 +0100

    Update V2.sol

--- V2.sol
+++ V2.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract SimpleFaucet {
+    constructor() payable {}
+
+    receive() external payable {}
+
+    function sendTo(address payable _to, uint _amount) public {
+        require(address(this).balance >= _amount, "Pas assez de fonds dans le faucet");
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Echec du transfert ETH");
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
