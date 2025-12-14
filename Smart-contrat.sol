5c7fe8b8609a347699dc934757a4668c13bc5883
Dimmis5 <zhangdimeo@gmail.com>
2025-12-14 15:34:28 +0100

    Refactor sendTo function for ETH transfer
    Updated the sendTo function to use call for ETH transfer and added event emission.

--- Smart-contrat.sol
+++ Smart-contrat.sol
@@ -2,13 +2,25 @@
 pragma solidity ^0.8.0;
 
 contract SimpleFaucet {
-    // Fonction pour recevoir de l'ETH
+    
+    // Event pour tracer les transferts
+    event Sent(address indexed to, uint amount);
+
+    // Constructeur pouvant recevoir des ETH à la création
+    constructor() payable {}
+
+    // Recevoir des ETH directement
     receive() external payable {}
 
-    // Fonction pour envoyer de l'ETH à quelqu'un
+    // Fonction pour envoyer des ETH depuis le faucet
     function sendTo(address payable _to, uint _amount) public {
         require(address(this).balance >= _amount, "Pas assez de fonds dans le faucet");
-        _to.transfer(_amount);
+        
+        (bool success, ) = _to.call{value: _amount}("");
+        require(success, "Echec du transfert ETH");
+
+        // Émettre l'event pour tracer le transfert
+        emit Sent(_to, _amount);
     }
 
     // Vérifier le solde du contrat
